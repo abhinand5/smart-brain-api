86f59b1a87134d0da1f869bf8053008b5a2e92b0
abhinand5 <abhinandalfio@gmail.com>
2018-07-18 14:30:19 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : '1999',
+    database : 'smart-brain'
+  }
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+app.get('/',(req, res) => { res.send(database.users); })
+
+app.post('/signin',(req, res) => {signin.handleSignin(req, res, db, bcrypt)} )
+
+app.post('/register', (req, res) =>{register.handleRegister(req, res, db, bcrypt) } )
+
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
+
+app.put('/image', (req, res) => {image.handleImage(req, res, db) })
+
+app.listen(3000, (req, res) => { console.log("Server is Up and Running on Port 3000."); });
+
+
+/* 
+<----------------- API MODEL --------------------->
+	/ [endpoint] --> Method => response
+	
+	/ (root) --> res => It's Working
+	/ signin --> POST => success/failure
+	/ register --> POST => user
+	/ profile /:userId --> GET = user
+	/ image --> PUT --> user
+*/
+
+
+
